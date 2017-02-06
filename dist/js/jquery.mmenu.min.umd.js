e78fa1dd9ceb0379e53df646e609da72f9c2cfdb
Olivier Bourdoux <olivier.bourdoux@gmail.com>
2017-02-06 11:37:12 +0100

    test

--- dist/js/jquery.mmenu.min.umd.js
+++ dist/js/jquery.mmenu.min.umd.js
@@ -10,7 +10,7 @@
 //If node environment, reassign jquery to a fake window using jsdom
 var isNode = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]';
 if (isNode) {
-	var jsdom = require('jsdom').jsdom,
+	var jsdom = require('jsdom').jsdom;
 	global.window = jsdom('').defaultView;
 	global.navigator = window.navigator;
 	jQuery = jQuery(window);
